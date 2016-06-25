a9879cd2b233d1f475e5c54ec9c8ec78e6bbb131
dmitriyr333 <dmitriyr333@gmail.com>
2016-06-25 17:10:21 -0400

    Create reverse_words.py

---
+++ reverse_words.py
@@ -0,0 +1,46 @@
+def rev_word1(s):
+    return " ".join(reversed(s.split()))
+
+#Or
+
+def rev_word2(s):
+    return " ".join(s.split()[::-1])
+
+''' 
+  long way, showing all algorithm
+'''
+def rev_word3(s):
+    """
+    Manually doing the splits on the spaces.
+    """
+    
+    words = []
+    length = len(s)
+    spaces = [' ']
+    
+    # Index Tracker
+    i = 0
+    
+    # While index is less than length of string
+    while i < length:
+        
+        # If element isn't a space
+        if s[i] not in spaces:
+            
+            # The word starts at this index
+            word_start = i
+            
+            while i < length and s[i] not in spaces:
+                
+                # Get index where word ends
+                i += 1
+            # Append that word to the list
+            words.append(s[word_start:i])
+        # Add to index
+        i += 1
+        
+    # Join the reversed words
+    return " ".join(reversed(words))
+    
+    
+rev_word1('Hi John,   are you ready to go?')
