6c3cb82f4e471d4c621c3d7aa143bcdf51299f37
dmitriyr333 <dmitriyr333@gmail.com>
2016-06-30 20:53:36 -0400

    Update Misc.py
    adding inheritance

--- Misc.py
+++ Misc.py
@@ -22,3 +22,25 @@ for index, value in enumerate(lst):
 '''
 for numb in xrange(10,0,-1):
 	print numb
+
+'''
+	Inheritance
+'''
+class Character:
+	def __init__(self, hair = None):
+		self.hair = hair
+
+	def __str__( self ):
+		return "Hair is: {0}.".format(self.hair)
+
+class Cook( Character ):
+	def __init__( self, hair = None, hat = None):
+		Character.__init__(self, hair)
+		self.hat = hat
+		
+	def __str__( self ):
+		return Character.__str__(self) + " And {0} hat.".format(self.hat)
+		
+
+mike = Cook('brown', 'pointy')
+print mike
