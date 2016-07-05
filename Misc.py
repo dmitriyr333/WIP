3628b91f93ad3e24812201916383c36c65d8cf11
dmitriyr333 <dmitriyr333@gmail.com>
2016-07-05 11:00:04 -0400

    Update Misc.py

--- Misc.py
+++ Misc.py
@@ -44,3 +44,26 @@ class Cook( Character ):
 
 mike = Cook('brown', 'pointy')
 print mike
+
+
+'''
+	Abstract Classes
+'''
+from abc import ABCMeta, abstractmethod
+class Book:
+    __metaclass__ = ABCMeta
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
+    
+#Write MyBook class
+class MyBook( Book ):
+    def __init__( self, title, author, price ):
+        Book.__init__( self, title, author )
+        self.price = price
+    def display( self ):
+        print "Title: " + title
+        print "Author: " + author
+        print "Price: " + str(self.price)
