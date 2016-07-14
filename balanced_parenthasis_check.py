3c73b064c558b8c9fd74b0099f08876ea38039b5
dmitriyr333 <dmitriyr333@gmail.com>
2016-07-14 17:57:23 -0400

    Create balanced_parenthasis_check.py

---
+++ balanced_parenthasis_check.py
@@ -0,0 +1,24 @@
+def balance_check( s ):
+	length = len(s)
+	
+	if length % 2 != 0:
+		return False
+	
+	stack   = []
+	opening = set( [ '(','[','{' ] )
+	match   = set( [ ('(',')'), ('[',']'), ('{','}') ] )
+	
+	for item in s:
+		if item in opening:
+			stack.append( item )
+		else:
+			prior_item = stack.pop()
+			if (prior_item, item) not in match:
+				return False
+				
+	return stack == []
+				
+        
+        
+'''test'''
+print balance_check('[](){([[[]]])}(')
