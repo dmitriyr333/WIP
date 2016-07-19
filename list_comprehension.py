0f7a7ae4cc7f7537db5fb5d03a11f6b5736b6d73
dmitriyr333 <dmitriyr333@gmail.com>
2016-07-19 16:45:12 -0400

    Create list_comprehension.py

---
+++ list_comprehension.py
@@ -0,0 +1,4 @@
+''' prb: input 4 values. Output a list of possible solutions that are less than n '''
+x, y, z, n = (int(raw_input()) for _ in range(4))
+
+print  [ [a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a+b+c != n ] 
