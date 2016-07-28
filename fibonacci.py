f86689f503082d299c26fca8478869013ac953d1
Dmitriy Rozentsvay <dmitriyr333@gmail.com>
2016-07-28 10:48:30 -0400

    Fibanacci, three implementations

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,6 @@
-# first, simple recursion
+# 0,1,1,2,3,5,8,13...
 
+# first, simple recursion
 def fib_simple_recursion( n ):
 
     # base case
@@ -10,4 +11,36 @@ def fib_simple_recursion( n ):
     else:
         return fib_simple_recursion(n-1) + fib_simple_recursion(n-2)
 
+
+# Dynamic programming:
+
+def fib_dynamic(n):
+
+    memo = dict()
+
+    if n < 2:
+        return n
+    else:
+        if n in memo:
+            return memo[n]
+        else:
+            memo[n] = fib_dynamic(n-1) + fib_dynamic(n-2)
+            return memo[n]
+
+
+
+# Iterate over
+def fib_iterate(n):
+    a = 0
+    b = 1
+
+    for _ in range(n):
+        a, b = b, a+b
+
+    return a
+
+
+''' test '''
 print fib_simple_recursion(10)
+print fib_dynamic(10)
+print fib_iterate(10)
