0a7d0dca9b525e2f43df58bddabbd8ecc88ab81e
Dmitriy Rozentsvay <dmitriyr333@gmail.com>
2016-07-29 10:40:56 -0400

    Add string permutation

--- balanced_parenthasis_check.py
+++ balanced_parenthasis_check.py
@@ -1,13 +1,15 @@
+# find if all parenthesis are in correct sequence
+
 def balance_check( s ):
 	length = len(s)
-	
+
 	if length % 2 != 0:
 		return False
-	
+
 	stack   = []
 	opening = set( [ '(','[','{' ] )
 	match   = set( [ ('(',')'), ('[',']'), ('{','}') ] )
-	
+
 	for item in s:
 		if item in opening:
 			stack.append( item )
@@ -15,10 +17,10 @@ def balance_check( s ):
 			prior_item = stack.pop()
 			if (prior_item, item) not in match:
 				return False
-				
+
 	return stack == []
-				
-        
-        
+
+
+
 '''test'''
 print balance_check('[](){([[[]]])}(')
