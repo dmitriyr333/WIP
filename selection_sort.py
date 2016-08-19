815d26bcf48585e78f9aea17576fc2a703b8bc08
Dmitriy Rozentsvay <dmitriyr333@gmail.com>
2016-08-19 15:28:16 -0400

    updated files

--- selection_sort.py
+++ selection_sort.py
@@ -1,25 +1,20 @@
-''' Yield is a keyword that is used like return, except the function will return a generator.'''
-'''
-    To master yield, you must understand that when you call the function, the code you have written in the function body does not run. The function only returns the generator object, this is a bit tricky :-)
+''' selection sort'''
 
-    Then, your code will be run each time the for uses the generator.
+def selection_sort( arr ):
+    
+    # first loop over all the elements
+    for i in range( len( arr) ) :
+        is_smallest = i
 
-    Now the hard part:
+        # second loop; starts only on first loop's index
+        for j in range( i, len(arr) ) :
+            if arr[j] < arr[is_smallest]:
+                is_smallest = j
 
-    The first time the for calls the generator object created from your function, it will run the code in your function from the beginning until it hits yield, then it'll return the first value of the loop. Then, each other call will run the loop you have written in the function one more time, and return the next value, until there is no value to return.
+        # switch values
+        arr[is_smallest], arr[i] = arr[i], arr[is_smallest]
 
-    The generator is considered empty once the function runs but does not hit yield anymore. It can be because the loop had come to an end, or because you do not satisfy an "if/else" anymore.
-'''
-def createGenerator():
-    mylist = range(3)
-    for i in mylist:
-        yield i*i
+    return arr
 
-mygenerator = createGenerator() # create a generator
-print(mygenerator) # mygenerator is an object!
-# generator object createGenerator at 0xb7555c34>
-for i in mygenerator:
-     print(i)
-#0
-#2
-#4
+if __name__ == '__main__' :
+    print selection_sort( [5,4,6,0,2] )
