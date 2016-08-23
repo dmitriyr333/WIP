35fa458d1703d5c9d69c118112743654a867aa27
Dmitriy Rozentsvay <dmitriyr333@gmail.com>
2016-08-23 06:37:42 -0400

    merge sort algo

---
+++ merge_sort.py
@@ -0,0 +1,26 @@
+def merge_sort( arr ):
+    out = []
+    if len(arr) < 2 :
+        return arr
+
+    mid = len(arr) // 2
+
+    left = merge_sort( arr[0:mid] )
+    right = merge_sort( arr[ mid: ] )
+    i = 0
+    j = 0
+
+    while i < len( left ) and j < len( right ):
+        if left[ i ] > right[ j ]:
+            out.append( right[j] )
+            j += 1
+        else:
+            out.append( left[ i ] )
+            i += 1
+    
+    out += left[i:]
+    out += right[j:]
+    
+    return out
+
+print merge_sort( [5,4,3,9,0] ) 
