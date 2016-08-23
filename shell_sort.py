ea3eacdeea708e64f6d4fecaeb556adeb8988fc8
Dmitriy Rozentsvay <dmitriyr333@gmail.com>
2016-08-23 05:43:18 -0400

    updated shell sort

--- shell_sort.py
+++ shell_sort.py
@@ -1,14 +1,15 @@
- def shellSort(array):
-     "Shell sort using Shell's (original) gap sequence: n/2, n/4, ..., 1."
-     gap = len(array) // 2
-     # loop over the gaps
-     while gap > 0:
-         # do the insertion sort
-         for i in range(gap, len(array)):
-             val = array[i]
-             j = i
-             while j >= gap and array[j - gap] > val:
-                 array[j] = array[j - gap]
-                 j -= gap
-             array[j] = val
-         gap //= 2
+def shellSort(array):
+    "Shell sort using Shell's (original) gap sequence: n/2, n/4, ..., 1."
+    gap = len(array) // 2
+    # loop over the gaps
+    while gap > 0:
+        # do the insertion sort
+        for i in range(gap, len(array)):
+            j = i
+            while j >= gap and array[j - gap] > array[j]:
+                array[j], array[j - gap] = array[j - gap], array[j]
+                j -= gap
+        gap //= 2
+    return array
+
+print shellSort([5,4,3,8,0])
